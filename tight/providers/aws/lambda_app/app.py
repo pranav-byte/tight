f69cb9bde3b99cc002a0377615d5164a53a1c1a9
michaelorionmcmanus@gmail.com <michaelorionmcmanus@gmail.com>
2016-12-12 10:30:49 -0800

    Add comments to providers.aws.lambda_app.app to clarify what  actually does.

--- tight/providers/aws/lambda_app/app.py
+++ tight/providers/aws/lambda_app/app.py
@@ -11,7 +11,61 @@ def run():
         info(message=e)
         traceback.print_exc()
 
+
 def create(current_module):
+    """ Attach functions to the app entry module.
+
+    Introspect the application function root and create function attributes on the provided
+    module that map to each application controller. An application controller
+    is defined as any directory in the app root that contains a `handler.py` file.
+    The name of the controller is the enclosing directory.
+
+    Given the following app structure:
+
+    app_index.py
+    app/
+        functions/
+            controller_a/
+                handler.py
+            controller_b/
+                handler.py
+            not_a_controller/
+                some_other_python.py
+
+    The controller names collected would be:
+
+    `controller_a` and `controller_b`
+
+    Notice that `not_a_controller` is omitted because there is no `handler.py` file in the directory.
+
+    Assuming that `app_index.py` is the module from which `create` is called, the result would  be that
+    `app_index.py` will behave as if it had been statically defined as:
+
+    def controller_a(controller_module_path, controller_name, event, context):
+        controller_module_path # 'app.functions.controller_a.handler'
+        controller_name # controller_a
+        callback = importlib.import_module(controller_module_path, 'handler')
+        return callback.handler(event, context, **kwargs)
+
+
+    def controller_b(controller_module_path, controller_name, event, context):
+        controller_module_path # 'app.functions.controller_b.handler'
+        controller_name # controller_b
+        callback = importlib.import_module(controller_module_path, 'handler')
+        return callback.handler(event, context, **kwargs)
+
+
+    This means that the handler value provided to lambda can follow the format:
+
+    'app_index.controller_a'
+    'app_index.controller_b'
+
+    So long as `app.functions.controller_a.handler` and `app.functions.controller_b.handler` define
+    functions that are decorated by `tight.providers.aws.controllers.lambda_proxy_event` the call to
+    `app_index.controller_a` or `app_index.controller_b` will in turn call the correct handler for the
+    request method by mapping `event['httpMethod']` to the correct module function.
+
+    """
     controllers = collect_controllers()
     for item in controllers:
         name, controller_module_path = item.popitem()
@@ -20,7 +74,6 @@ def create(current_module):
             func_args = args[1:4]
             callback = importlib.import_module(controller_module_path, 'handler')
             return callback.handler(*func_args, **kwargs)
-
         bound_function = partial(function, *(controller_module_path, name))
         function.__name__ = name + '_module'
         setattr(current_module, name, bound_function)
