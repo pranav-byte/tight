9a2cdf77c8f42bab19846948c5c378c075594bef
michaelorionmcmanus@gmail.com <michaelorionmcmanus@gmail.com>
2016-12-25 12:26:04 -0800

    Placebos are placed in a test specific subdirectory of placebos so that they can be automatically cleared during recording.

--- tight/core/test_helpers.py
+++ tight/core/test_helpers.py
@@ -1,4 +1,4 @@
-import pytest, os, sys, importlib, yaml, json
+import pytest, os, sys, importlib, yaml, json, shutil
 from botocore import session as boto_session
 from tight.providers.aws.clients import boto3_client
 from tight.providers.aws.clients import dynamo_db
@@ -14,34 +14,43 @@ def event():
         event = yaml.load(data_file)
     return event
 
-def placebos_path(file):
+def placebos_path(file, namespace, mode='playback'):
     test_path = '/'.join(file.split('/')[0:-1])
-    path = '/'.join([test_path, 'placebos'])
-    return path
+    placebo_path = '/'.join([test_path, 'placebos'])
+    namespaced_path = '{}/{}'.format(placebo_path, namespace)
+    if mode == 'record':
+        if os.path.exists(namespaced_path):
+            shutil.rmtree(namespaced_path)
+            os.mkdir(namespaced_path)
+        else:
+            os.mkdir(namespaced_path)
+    return namespaced_path
 
-def spy_on_session(file, session):
-    pill = placebo.attach(session, data_path=placebos_path(file))
+def spy_on_session(file, session, placebo_path):
+    pill = placebo.attach(session, data_path=placebo_path)
     return pill
 
-def record(file, dynamo_db_session):
+def record(file, dynamo_db_session, namespace):
     this = sys.modules[__name__]
+    placebo_path = placebos_path(file, namespace, mode='record')
     if not hasattr(this, 'pill') or not hasattr(this, 'boto3_pill'):
         boto3_session = boto3_client.session()
-        boto3_pill = spy_on_session(file, boto3_session)
+        boto3_pill = spy_on_session(file, boto3_session, placebo_path)
         boto3_pill.record()
-        pill = spy_on_session(file, dynamo_db_session)
+        pill = spy_on_session(file, dynamo_db_session, placebo_path)
         os.environ['RECORD'] = 'True'
         pill.record()
         setattr(this, 'pill', pill)
         setattr(this, 'boto3_pill', boto3_pill)
 
-def playback(file, dynamo_db_session):
+def playback(file, dynamo_db_session, namespace):
     this = sys.modules[__name__]
+    placebo_path = placebos_path(file, namespace, mode='playback')
     if not hasattr(this, 'pill') or not hasattr(this, 'boto3_pill'):
         boto3_session = boto3_client.session()
-        boto3_pill = spy_on_session(file, boto3_session)
+        boto3_pill = spy_on_session(file, boto3_session, placebo_path)
         boto3_pill.playback()
-        pill = spy_on_session(file, dynamo_db_session)
+        pill = spy_on_session(file, dynamo_db_session, placebo_path)
         os.environ['PLAYBACK'] = 'True'
         pill.playback()
         setattr(this, 'pill', pill)
