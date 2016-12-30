06e13912a4c20217a9bb90a76cd56ff9b77360c6
michaelorionmcmanus@gmail.com <michaelorionmcmanus@gmail.com>
2016-12-29 17:08:26 -0800

    Comments and code coverage.

--- tight/core/logger.py
+++ tight/core/logger.py
@@ -5,5 +5,12 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def info(*args, **kwargs):
+    """
+    Log a message using the system logger.
+
+    :param args:
+    :param kwargs:
+    :return: None
+    """
     message = kwargs.pop('message')
     logger.info(message)
