067e0d51c56c051c7eb7bd2b304efe8fe886ccd5
michaelorionmcmanus@gmail.com <michaelorionmcmanus@gmail.com>
2017-01-18 15:29:53 -0800

    lululemon athletica Canada inc. is copyright holder.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2016 Michael Orion McManus
+# Copyright (c) 2017 lululemon athletica Canada inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
